ae035b5567077c597cb996748b1a8baed1fd7277
eduardomingoranca <eduardomingoranca@gmail.com>
2023-01-23 06:08:28 -0300

    Mudando o comportamento da aplicacao com Spring Profiles

--- algafood/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
+++ algafood/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
@@ -7,19 +7,19 @@ import org.springframework.stereotype.Component;
 import static com.algaworks.algafood.di.notificacao.NivelUrgencia.SEM_URGENCIA;
 
 
-@Profile("prod")
+@Profile("dev")
 @TipoDoNotificador(SEM_URGENCIA)
 @Component
-public class NotificadorEmail implements Notificador {
-    public NotificadorEmail() {
-        System.out.println("NotificadorEmail REAL");
+public class NotificadorEmailMock implements Notificador {
+
+    public NotificadorEmailMock() {
+        System.out.println("NotificadorEmail MOCK");
     }
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        System.out.printf("Notificando %s atraves do email %s: %s\n",
+        System.out.printf("MOCK: Notificacao seria enviada para %s atraves do email %s: %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
     }
-
 
 }
