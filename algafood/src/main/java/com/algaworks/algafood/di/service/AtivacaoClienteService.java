28f02b96437dda536e334d6ce40a4a14e32fc95d
eduardomingoranca <eduardomingoranca@gmail.com>
2023-01-23 06:33:18 -0300

    Criando metodos de callback do ciclo de vida dos beans 02

--- algafood/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ algafood/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -3,34 +3,34 @@ package com.algaworks.algafood.di.service;
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.Notificador;
 import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import static com.algaworks.algafood.di.notificacao.NivelUrgencia.SEM_URGENCIA;
 
 @Component
-public class AtivacaoClienteService implements InitializingBean, DisposableBean {
+public class AtivacaoClienteService {
 
     @TipoDoNotificador(SEM_URGENCIA)
     @Autowired
     private Notificador notificador;
 
+    @PostConstruct
+    public void init() {
+        System.out.println("INIT " + notificador);
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("DESTROY");
+    }
+
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
         notificador.notificar(cliente, "Seu cadastro no sistema esta ativo!");
-    }
-
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("DESTROY");
-    }
-
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("INIT " + notificador);
     }
 
 }
