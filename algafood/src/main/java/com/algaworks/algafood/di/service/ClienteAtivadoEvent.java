6da3d8051ad3404a449d5198cd3cedf9a2e2dc7b
eduardomingoranca <eduardomingoranca@gmail.com>
2023-01-23 21:21:55 -0300

    Adicionando JPA e configurando o Data Source

--- algafood/src/main/java/com/algaworks/algafood/di/service/ClienteAtivadoEvent.java
+++
@@ -1,9 +0,0 @@
-package com.algaworks.algafood.di.service;
-
-import com.algaworks.algafood.di.modelo.Cliente;
-
-public record ClienteAtivadoEvent(Cliente cliente) {
-
-    // apenas um atributo / construtor / getter -> record
-
-}
