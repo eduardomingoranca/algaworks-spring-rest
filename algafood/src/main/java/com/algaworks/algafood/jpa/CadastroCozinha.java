3a7ac0db2df4e85dd62d807e2befd3e4574a9a7d
eduardomingoranca <eduardomingoranca@gmail.com>
2023-01-24 07:42:28 -0300

    Adicionando um objeto no banco de dados

--- algafood/src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
+++ algafood/src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.jpa;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -16,6 +17,11 @@ public class CadastroCozinha {
     public List<Cozinha> listar() {
         // utilizando o JPQL para uma consulta da tabela cozinha no banco de dados
         return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
+    }
+
+    @Transactional
+    public Cozinha adicionar(Cozinha cozinha) {
+        return manager.merge(cozinha);
     }
 
 }
