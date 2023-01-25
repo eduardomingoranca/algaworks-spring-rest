89bafe3a345d9e57b3bdba4996f434ddbe957bf5
eduardomingoranca <eduardomingoranca@gmail.com>
2023-01-25 15:58:09 -0300

    Definindo o status da resposta HTTP com @ResponseStatus

--- algafood/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -4,13 +4,11 @@ import com.algaworks.algafood.api.model.CozinhasXMLWrapper;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
 
@@ -33,6 +31,7 @@ public class CozinhaController {
     }
 
     // cozinhas/{cozinhaId}
+    @ResponseStatus(CREATED)
     @GetMapping("/{cozinhaId}")
     public Cozinha buscar(@PathVariable Long cozinhaId) {
         return cozinhaRepository.porID(cozinhaId);
