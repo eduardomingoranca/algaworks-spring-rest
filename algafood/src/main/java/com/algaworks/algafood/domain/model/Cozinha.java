ab997edfd00d2dc980641165c5166ed8ff51a96a
eduardomingoranca <eduardomingoranca@gmail.com>
2023-01-25 06:39:55 -0300

    Conhecendo e usando o Lombok

--- algafood/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ algafood/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,14 +1,19 @@
 package com.algaworks.algafood.domain.model;
 
-import javax.persistence.*;
-import java.util.Objects;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import static java.util.Objects.hash;
+import javax.persistence.*;
+
 import static javax.persistence.GenerationType.IDENTITY;
 
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "cozinha")
 public class Cozinha {
+
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
@@ -16,32 +21,4 @@ public class Cozinha {
     @Column(name = "nome")
     private String nome;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cozinha cozinha = (Cozinha) o;
-        return Objects.equals(id, cozinha.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return hash(id);
-    }
 }
