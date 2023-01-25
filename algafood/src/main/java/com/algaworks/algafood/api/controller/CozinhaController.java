df8d8f3f7542ede39fcd39529a89493c9cace9cb
eduardomingoranca <eduardomingoranca@gmail.com>
2023-01-25 15:20:06 -0300

    Consultando Singleton Resource com GET e @PathVariable

--- algafood/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -4,6 +4,7 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +31,12 @@ public class CozinhaController {
     public List<Cozinha> listar2() {
         System.out.println("LISTAR 2");
         return cozinhaRepository.todas();
+    }
+
+    // cozinhas/{cozinhaId}
+    @GetMapping("/{cozinhaId}")
+    public Cozinha buscar(@PathVariable Long cozinhaId) {
+        return cozinhaRepository.porID(cozinhaId);
     }
 
 }
