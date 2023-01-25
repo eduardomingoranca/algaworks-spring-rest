2d858d6b1a34373601ad1efe871878dd7bb324fb
eduardomingoranca <eduardomingoranca@gmail.com>
2023-01-25 16:20:02 -0300

    Manipulando a resposta HTTP com ResponseEntity 02

--- algafood/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -4,15 +4,18 @@ import com.algaworks.algafood.api.model.CozinhasXMLWrapper;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.http.HttpHeaders.LOCATION;
+import static org.springframework.http.HttpStatus.FOUND;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
-import static org.springframework.http.ResponseEntity.ok;
+import static org.springframework.http.ResponseEntity.status;
 
 @RestController
 //@RequestMapping(value = "/cozinhas", produces = APPLICATION_JSON_VALUE)
@@ -32,12 +35,17 @@ public class CozinhaController {
         return new CozinhasXMLWrapper(cozinhaRepository.todas());
     }
 
-
     // ResponseEntity permite que customize a resposta HTTP
     @GetMapping("/{cozinhaId}")
     public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
-        Cozinha cozinha = cozinhaRepository.porID(cozinhaId);
-        return ok(cozinha);
+        cozinhaRepository.porID(cozinhaId);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(LOCATION, "http://127.0.0.1:8080/cozinhas");
+
+        return status(FOUND)
+                .headers(headers)
+                .build();
     }
 
 }
