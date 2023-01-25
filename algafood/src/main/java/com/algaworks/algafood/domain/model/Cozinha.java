f1e8c37f32028b3640ad19229887766bc2c58f71
eduardomingoranca <eduardomingoranca@gmail.com>
2023-01-25 15:33:36 -0300

    Customizando as representacoes XML e JSON com @JsonIgnore, @JsonProperty e @JsonRootName

--- algafood/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ algafood/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,5 +1,8 @@
 package com.algaworks.algafood.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -7,18 +10,21 @@ import javax.persistence.*;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
+@JsonRootName("gastronomia")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "cozinha")
 public class Cozinha {
 
+    @JsonIgnore
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
-    @Column(name = "nome", nullable = false)
+    @JsonProperty("titulo")
+    @Column(nullable = false)
     private String nome;
 
 }
