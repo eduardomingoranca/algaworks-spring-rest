d101a4f7c822308b233d9e3ac63975094aa88573
eduardomingoranca <eduardomingoranca@gmail.com>
2023-01-25 16:09:13 -0300

    Manipulando a resposta HTTP com ResponseEntity

--- algafood/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -4,13 +4,15 @@ import com.algaworks.algafood.api.model.CozinhasXMLWrapper;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
+import static org.springframework.http.ResponseEntity.status;
 
 @RestController
 //@RequestMapping(value = "/cozinhas", produces = APPLICATION_JSON_VALUE)
@@ -19,22 +21,23 @@ public class CozinhaController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
-    // GET /cozinhas HTTP/1.1
     @GetMapping(produces = APPLICATION_JSON_VALUE)
     public List<Cozinha> listar() {
         return cozinhaRepository.todas();
     }
 
+    @ResponseStatus(OK)
     @GetMapping(produces = APPLICATION_XML_VALUE)
     public CozinhasXMLWrapper listarXML() {
         return new CozinhasXMLWrapper(cozinhaRepository.todas());
     }
 
-    // cozinhas/{cozinhaId}
-    @ResponseStatus(CREATED)
+
+    // ResponseEntity permite que customize a resposta HTTP
     @GetMapping("/{cozinhaId}")
-    public Cozinha buscar(@PathVariable Long cozinhaId) {
-        return cozinhaRepository.porID(cozinhaId);
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
+        Cozinha cozinha = cozinhaRepository.porID(cozinhaId);
+        return status(OK).body(cozinha);
     }
 
 }
