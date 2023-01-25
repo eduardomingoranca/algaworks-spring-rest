16ec3e972f39462e2a0a6dc1ccbc3b54dad0dd2c
eduardomingoranca <eduardomingoranca@gmail.com>
2023-01-25 16:31:10 -0300

    Corrigindo o Status HTTP para resource inexistente 01

--- algafood/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -4,17 +4,15 @@ import com.algaworks.algafood.api.model.CozinhasXMLWrapper;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-import static org.springframework.http.HttpHeaders.LOCATION;
-import static org.springframework.http.HttpStatus.FOUND;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
+import static org.springframework.http.ResponseEntity.notFound;
 import static org.springframework.http.ResponseEntity.status;
 
 @RestController
@@ -35,17 +33,14 @@ public class CozinhaController {
         return new CozinhasXMLWrapper(cozinhaRepository.todas());
     }
 
-    // ResponseEntity permite que customize a resposta HTTP
     @GetMapping("/{cozinhaId}")
     public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
-        cozinhaRepository.porID(cozinhaId);
+        Cozinha cozinha = cozinhaRepository.porID(cozinhaId);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(LOCATION, "http://127.0.0.1:8080/cozinhas");
+        if (cozinha != null)
+            return status(OK).body(cozinha);
 
-        return status(FOUND)
-                .headers(headers)
-                .build();
+        return notFound().build();
     }
 
 }
