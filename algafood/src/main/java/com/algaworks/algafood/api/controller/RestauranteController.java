6869180b30fcb63e6a23133544745a02f88b485a
eduardomingoranca <eduardomingoranca@gmail.com>
2023-01-27 06:44:57 -0300

    Finalizando a atualizacao parcial com a API de Reflections do Spring

--- algafood/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ algafood/src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -4,16 +4,19 @@ import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
 import static org.springframework.beans.BeanUtils.copyProperties;
 import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.ResponseEntity.status;
+import static org.springframework.util.ReflectionUtils.*;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -79,9 +82,23 @@ public class RestauranteController {
     }
 
     // mesclar os valores do map campos para dentro do restaurante atual
-    private static void merge(Map<String, Object> camposOrigem, Restaurante restauranteDestino) {
-        camposOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-            System.out.println(nomePropriedade + " = " + valorPropriedade);
+    private static void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
+        // ObjectMapper -> class responsavel por converter objetos java em json e json em objetos java
+        ObjectMapper objectMapper = new ObjectMapper();
+        Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+
+        dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+            // obtendo os campos de restaurante e informado no nome da propriedade informada
+            Field field = findField(Restaurante.class, nomePropriedade);
+            // acessando um atributo/metodo private
+            assert field != null;
+            field.setAccessible(true);
+
+            // obtendo o valor da propriedade convertido
+            Object novoValor = getField(field, restauranteOrigem);
+
+            // alterando o valor da variavel de instancia informada pelo valor da propriedade
+            setField(field, restauranteDestino, novoValor);
         });
     }
 
