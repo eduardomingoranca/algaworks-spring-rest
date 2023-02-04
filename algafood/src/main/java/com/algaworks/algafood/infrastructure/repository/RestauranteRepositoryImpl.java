9edcaeb3ce4ca69748239f1dda3c7639b62643d3
eduardomingoranca <eduardomingoranca@gmail.com>
2023-02-04 09:37:13 -0300

    Adicionando restricoes na clausula where com Criteria API

--- algafood/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ algafood/src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -9,6 +9,8 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -26,7 +28,19 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
         // criteria cria a query java de forma programatica
         // construtor de clausura
         CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
-        criteria.from(Restaurante.class); // from Restaurante
+        Root<Restaurante> root  = criteria.from(Restaurante.class);// from Restaurante
+
+        // predicate -> eh um criterio/um filtro
+        Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%"); // like %:nome%
+
+        Predicate taxaInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"),
+                taxaFreteInicial); // taxaFrete >= :taxaFreteInicial
+
+        Predicate taxaFinalPredicate = builder.lessThanOrEqualTo(root.get("taxaFrete"),
+                taxaFreteFinal); // taxaFrete <= :taxaFreteFinal
+
+        // where nome like %:nome% and taxaFrete >= :taxaFreteInicial and taxaFrete <= :taxaFreteFinal
+        criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
 
         // criando a query sql
         TypedQuery<Restaurante> query = manager.createQuery(criteria);
