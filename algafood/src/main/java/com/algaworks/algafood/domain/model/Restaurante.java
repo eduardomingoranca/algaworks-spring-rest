41b21c53afd1d91592b66e30f18d6f26a3e58575
eduardomingoranca <eduardomingoranca@gmail.com>
2023-02-05 09:43:59 -0300

    Mapeando relacionamento muitos-para-muitos com @ManyToMany

--- algafood/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ algafood/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -5,6 +5,8 @@ import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
@@ -27,5 +29,12 @@ public class Restaurante {
     @ManyToOne
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
+
+    // joinColumns -> define qual o nome da coluna da chave estrangeira da tabela intermediaria
+    @ManyToMany
+    @JoinTable(name = "restaurante_forma_pagamento",
+    joinColumns = @JoinColumn(name = "restaurante_id"),
+    inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+    private List<FormaPagamento> formasPagamento = new ArrayList<>();
 
 }
