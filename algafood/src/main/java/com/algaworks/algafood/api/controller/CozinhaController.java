4f71a5850e6a27153c949ad2c6c64dbbbf96c654
eduardomingoranca <eduardomingoranca@gmail.com>
2023-02-11 10:42:56 -0300

    Lancando excecoes do tipo ResponseStatusException

--- algafood/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ algafood/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -1,11 +1,13 @@
 package com.algaworks.algafood.api.controller;
 
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -68,7 +70,11 @@ public class CozinhaController {
     @DeleteMapping("/{cozinhaId}")
     @ResponseStatus(NO_CONTENT)
     public void remover(@PathVariable Long cozinhaId) {
-        cadastroCozinha.excluir(cozinhaId);
+        try {
+            cadastroCozinha.excluir(cozinhaId);
+        } catch (EntidadeNaoEncontradaException e) {
+            throw new ResponseStatusException(NOT_FOUND, e.getMessage());
+        }
     }
 
 }
