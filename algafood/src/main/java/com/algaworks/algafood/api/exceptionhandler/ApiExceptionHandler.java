4750bb50cf9feaa25e9f6b86f52ada5af2de16c1
eduardomingoranca <eduardomingoranca@gmail.com>
2023-02-12 14:24:59 -0300

    Criando um exception handler global com ResponseEntityExceptionHandler

--- algafood/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ algafood/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -4,9 +4,9 @@ import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
@@ -16,7 +16,7 @@ import static org.springframework.http.ResponseEntity.status;
 // @ControllerAdvice -> anotacao informa que dentro deste componente pode-se adicionar
 // as exception handler que as exceptions que todos os controllers do projeto serao tratadas por aqui.
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     // metodo responsavel por tratar e capturar exceptions dos controllers
     // ExceptionHandler -> informa a classe e excecao que tera o tratamento e o retorno.
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
@@ -37,16 +37,6 @@ public class ApiExceptionHandler {
                 .build();
 
         return status(BAD_REQUEST).body(problema);
-    }
-
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public ResponseEntity<Object> tratarHttpMediaTypeNotSupportedException() {
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem("O tipo de media nao eh aceito.")
-                .build();
-
-        return status(UNSUPPORTED_MEDIA_TYPE).body(problema);
     }
 
     @ExceptionHandler(EntidadeEmUsoException.class)
