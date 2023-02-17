adeb2d1c1605459b345e3fedb27eb71212e1c2bc
eduardomingoranca <eduardomingoranca@gmail.com>
2023-02-17 19:49:48 -0300

    Estendendo o Problem Details para adicionar as propriedades com constraints violadas

--- algafood/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ algafood/src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,7 +20,10 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 import static com.algaworks.algafood.api.exceptionhandler.ProblemType.*;
 import static java.lang.String.format;
@@ -139,7 +143,19 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         String detail = "Um ou mais campos estao invalidos. Faca o preenchimento correto e " +
                 "tente novamente.";
 
+//        BindingResult -> armazena as violacoes de constraints de validacao, ou seja,
+//        dentro dessa classe pode-se ter o acesso a quais fields/propriedades foram violadas
+        BindingResult bindingResult = ex.getBindingResult();
+
+//        construindo o response para as propriedades violadas.
+        List<Problem.Field> problemFields = bindingResult.getFieldErrors()
+                .stream().map(fieldError -> Problem.Field.builder()
+                        .name(fieldError.getField())
+                        .userMessage(fieldError.getDefaultMessage())
+                        .build()).collect(Collectors.toList());
+
         Problem problem = createProblemBuilder(status, DADOS_INVALIDOS, detail, detail)
+                .fields(problemFields)
                 .build();
 
         return this.handleExceptionInternal(ex, problem, headers, status, request);
--- algafood/src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
+++ algafood/src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 // so incluia na representacao json se o valor nao estiver nulo.
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -15,6 +16,8 @@ public class Problem {
     // status -> o codigo de status HTTP.
     private Integer status;
 
+    private LocalDateTime timestamp;
+
     // type -> URI que especifica o tipo do problema.
     private String type;
 
@@ -24,8 +27,16 @@ public class Problem {
     // detail -> descricao especifica sobre a ocorrencia do erro.
     private String detail;
 
-
     private String userMessage;
-    private LocalDateTime timestamp;
+
+//    lista de propriedades violadas
+    private List<Field> fields;
+
+    @Getter
+    @Builder
+    public static class Field {
+        private String name;
+        private String userMessage;
+    }
 
 }
