b31fd67828f11913beb528f73254d0cc5d8c9c8d
eduardomingoranca <eduardomingoranca@gmail.com>
2023-02-19 09:28:32 -0300

    Conhecendo e adicionando mais constraints de validacao no modelo

--- algafood/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ algafood/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -7,7 +7,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -25,9 +25,14 @@ public class Restaurante {
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
-    @NotNull
+//    @NotNull
+//    @NotEmpty // nao aceita valor vazio
+    @NotBlank // nao pode ser nulo, vazio e em branco
     @Column(nullable = false)
     private String nome;
+
+//    @DecimalMin("0")
+    @PositiveOrZero // permitido apenas valor positivo ou zero
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
