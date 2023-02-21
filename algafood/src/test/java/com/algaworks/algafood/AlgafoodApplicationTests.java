b01fda02f8a18c2ecb3b0a515b2216fdfc06ef80
eduardomingoranca <eduardomingoranca@gmail.com>
2023-02-21 07:05:55 -0300

    Preparando o projeto para testes de integracao

--- algafood/src/test/java/com/algaworks/algafood/AlgafoodApplicationTests.java
+++ algafood/src/test/java/com/algaworks/algafood/AlgafoodApplicationTests.java
@@ -3,11 +3,15 @@ package com.algaworks.algafood;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+// @SpringBootTest -> fornece as funcionalidades do spring nos testes
 @SpringBootTest
 class AlgafoodApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertFalse(true);
 	}
 
 }
