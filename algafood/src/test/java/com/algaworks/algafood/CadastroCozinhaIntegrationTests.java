c6a7bb424b3c756052eefa858ee070ee4b2c6d29
eduardomingoranca <eduardomingoranca@gmail.com>
2023-02-21 08:58:33 -0300

    Desafio: escrevendo testes de integracao

--- algafood/src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
+++ algafood/src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
@@ -1,8 +1,7 @@
 package com.algaworks.algafood;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
+import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 import org.junit.jupiter.api.Test;
@@ -10,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.validation.ConstraintViolationException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 // @SpringBootTest -> fornece as funcionalidades do spring nos testes
@@ -41,6 +43,24 @@ class CadastroCozinhaIntegrationTests {
         ConstraintViolationException erroEsperado =
                 assertThrows(ConstraintViolationException.class, () ->
                         cadastroCozinha.salvar(novaCozinha));
+
+        assertThat(erroEsperado).isNotNull();
+    }
+
+    @Test
+    void deveFalharQuandoExcluirCozinhaEmUso() {
+        EntidadeEmUsoException erroEsperado =
+                assertThrows(EntidadeEmUsoException.class, () ->
+                        cadastroCozinha.excluir(1L));
+
+        assertThat(erroEsperado).isNotNull();
+    }
+
+    @Test
+    void deveFalharQuandoExcluirCozinhaInexistente() {
+        CozinhaNaoEncontradaException erroEsperado =
+                assertThrows(CozinhaNaoEncontradaException.class, () ->
+                        cadastroCozinha.excluir(100L));
 
         assertThat(erroEsperado).isNotNull();
     }
