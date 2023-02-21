0cc480924b95024083bf0b148e126052edcd94ff
eduardomingoranca <eduardomingoranca@gmail.com>
2023-02-21 10:09:05 -0300

    Validando o corpo da resposta HTTP

--- algafood/src/test/java/com/algaworks/algafood/CadastroCozinhaAPITests.java
+++ algafood/src/test/java/com/algaworks/algafood/CadastroCozinhaAPITests.java
@@ -1,12 +1,14 @@
 package com.algaworks.algafood;
 
-import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 
+import static io.restassured.RestAssured.enableLoggingOfRequestAndResponseIfValidationFails;
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.http.HttpStatus.OK;
 
 // subindo um servidor web
@@ -20,7 +22,7 @@ class CadastroCozinhaAPITests {
     @Test
     void deveRetornarStatus200QuandoConsultarCozinhas() {
         // verificando a resposta quando ocorre o erro no teste
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        enableLoggingOfRequestAndResponseIfValidationFails();
 
         // Rest Assured -> biblioteca para teste e validacao de rest apis
         // dado que o caminho /cozinhas com a porta 8080 aceitando um retorno em json
@@ -34,6 +36,21 @@ class CadastroCozinhaAPITests {
         // entao o status que deve retornar eh o 200
            .then()
                 .statusCode(OK.value());
+    }
+
+    @Test
+    void deveConterQuatroCozinhasQuandoConsultarCozinhas() {
+        given()
+                .basePath("/cozinhas")
+                .port(port)
+                .accept(JSON)
+        .when()
+                .get()
+        .then()
+                // hamcrest -> biblioteca para escrever expressoes
+                // com regras de correspondencia entre objetos
+                .body("", hasSize(4))
+                .body("nome", hasItems("Indiana", "Tailandesa"));
     }
 
 }
