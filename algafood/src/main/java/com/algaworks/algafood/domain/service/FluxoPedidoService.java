bbbda42d37102c1f30d182c8830d1b34fd35ff0c
eduardomingoranca <eduardomingoranca@gmail.com>
2023-04-02 10:33:44 -0300

    Usando o servico de envio de e-mails na confirmacao de pedidos

--- algafood/src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
+++ algafood/src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.domain.service;
 
 import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.service.mail.EnvioEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,10 +11,21 @@ public class FluxoPedidoService {
     @Autowired
     private EmissaoPedidoService emissaoPedido;
 
+    @Autowired
+    private EnvioEmailService envioEmail;
+
     @Transactional
     public void confirmar(String codigo) {
         Pedido pedido = emissaoPedido.buscarOuFalhar(codigo);
         pedido.confirmar();
+
+        EnvioEmailService.Mensagem mensagem = EnvioEmailService.Mensagem.builder()
+                .assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+                .corpo("O pedido de codigo <strong>" + pedido.getCodigo() + "</strong> foi confirmado!")
+                .destinatario(pedido.getCliente().getEmail())
+                .build();
+
+        envioEmail.enviar(mensagem);
     }
 
     @Transactional
--- algafood/src/main/resources/db/testdata/afterMigrate.sql
+++ algafood/src/main/resources/db/testdata/afterMigrate.sql
@@ -10,9 +10,9 @@ delete from permissao;
 delete from produto;
 delete from restaurante;
 delete from restaurante_forma_pagamento;
+delete from restaurante_usuario_responsavel;
 delete from usuario;
 delete from usuario_grupo;
-delete from restaurante_usuario_responsavel;
 delete from pedido;
 delete from item_pedido;
 delete from foto_produto;
@@ -31,7 +31,6 @@ alter table usuario auto_increment = 1;
 alter table pedido auto_increment = 1;
 alter table item_pedido auto_increment = 1;
 
-
 insert into cozinha (id, nome) values (1, 'Tailandesa');
 insert into cozinha (id, nome) values (2, 'Indiana');
 insert into cozinha (id, nome) values (3, 'Argentina');
@@ -47,19 +46,12 @@ insert into cidade (id, nome, estado_id) values (3, 'Sao Paulo', 2);
 insert into cidade (id, nome, estado_id) values (4, 'Campinas', 2);
 insert into cidade (id, nome, estado_id) values (5, 'Fortaleza', 3);
 
-insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo, aberto,
-endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro) values (1, 'Thai Gourmet', 10,
- 1, utc_timestamp, utc_timestamp, true, true, 1, '38400-999', 'Rua John Parr', '1000', 'Centro');
-insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo, aberto) values (2,
-'Thai Delivery', 9.50, 1, utc_timestamp, utc_timestamp, true, true);
-insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo, aberto) values (3,
-'Tuk Tuk Comida Indiana', 15, 2, utc_timestamp, utc_timestamp, true, true);
-insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo, aberto) values (4,
-'Java Steakhouse', 12, 3, utc_timestamp, utc_timestamp, true, true);
-insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo, aberto) values (5,
-'Lanchonete do Tio Sam', 11, 4, utc_timestamp, utc_timestamp, true, true);
-insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo, aberto) values (6,
-'Bar da Maria', 6, 4, utc_timestamp, utc_timestamp, true, true);
+insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo, aberto, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_bairro) values (1, 'Thai Gourmet', 10, 1, utc_timestamp, utc_timestamp, true, true, 1, '38400-999', 'Rua Joao Pinheiro', '1000', 'Centro');
+insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo, aberto) values (2, 'Thai Delivery', 9.50, 1, utc_timestamp, utc_timestamp, true, true);
+insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo, aberto) values (3, 'Tuk Tuk Comida Indiana', 15, 2, utc_timestamp, utc_timestamp, true, true);
+insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo, aberto) values (4, 'Java Steakhouse', 12, 3, utc_timestamp, utc_timestamp, true, true);
+insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo, aberto) values (5, 'Lanchonete do Tio Sam', 11, 4, utc_timestamp, utc_timestamp, true, true);
+insert into restaurante (id, nome, taxa_frete, cozinha_id, data_cadastro, data_atualizacao, ativo, aberto) values (6, 'Bar da Maria', 6, 4, utc_timestamp, utc_timestamp, true, true);
 
 insert into forma_pagamento (id, descricao) values (1, 'Cartao de credito');
 insert into forma_pagamento (id, descricao) values (2, 'Cartao de debito');
@@ -68,46 +60,48 @@ insert into forma_pagamento (id, descricao) values (3, 'Dinheiro');
 insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
 insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
 
-insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3),
-(3, 2), (3, 3), (4, 1), (4, 2), (5, 1), (5, 2), (6, 3);
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3), (4, 1), (4, 2), (5, 1), (5, 2), (6, 3);
 
-insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Porco com molho agridoce',
-'Deliciosa carne suina ao molho especial', 78.90, 0, 1);
-insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Camarao tailandes',
-'16 camaroes grandes ao molho picante', 110, 1, 1);
-insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Salada picante com carne grelhada',
-'Salada de folhas com cortes finos de carne bovina grelhada e nosso molho especial de pimenta vermelha', 87.20, 1, 2);
-insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Garlic Naan',
-'Pao tradicional indiano com cobertura de alho', 21, 1, 3);
-insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Murg Curry',
-'Cubos de frango preparados com molho curry e especiarias', 43, 0, 3);
-insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Bife Ancho',
-'Corte macio e suculento, com dois dedos de espessura, retirado da parte dianteira do contrafile', 79, 1, 4);
-insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('T-Bone',
-'Corte muito saboroso, com um osso em formato de T, sendo de um lado o contrafile e do outro o file mignon', 89, 0, 4);
-insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Sanduiche X-Tudo',
-'Sandubao com muito queijo, hamburger bovino, bacon, ovo, salada e maionese', 19, 1, 5);
-insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Espetinho de Cupim',
-'Acompanha farinha, mandioca e vinagrete', 8, 1, 6);
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Porco com molho agridoce', 'Deliciosa carne suina ao molho especial', 78.90, 0, 1);
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Camarao tailandês', '16 camaroes grandes ao molho picante', 110, 1, 1);
 
-insert into grupo (id, nome) values (1, 'Gerencia'), (2, 'Vendas'), (3, 'Secretaria'), (4, 'Cadastramento');
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Salada picante com carne grelhada', 'Salada de folhas com cortes finos de carne bovina grelhada e nosso molho especial de pimenta vermelha', 87.20, 1, 2);
 
-insert into usuario (id, nome, email, senha, data_cadastro) values (1, 'John Smith', 'john.ger@algafood.com', '123',
-utc_timestamp), (2, 'Mary Johnson', 'mary.vnd@algafood.com', '123', utc_timestamp),
-(3, 'Joseph Smith', 'joseph.aux@algafood.com', '123', utc_timestamp),(4, 'Sebastian Marley', 'sebastian.cad@algafood.com',
-'123', utc_timestamp);
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Garlic Naan', 'Pao tradicional indiano com cobertura de alho', 21, 1, 3);
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Murg Curry', 'Cubos de frango preparados com molho curry e especiarias', 43, 1, 3);
 
-insert into grupo_permissao (grupo_id, permissao_id) values (1, 1), (1, 2), (2, 1), (2, 2), (3, 1);
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Bife Ancho', 'Corte macio e suculento, com dois dedos de espessura, retirado da parte dianteira do contrafile', 79, 1, 4);
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('T-Bone', 'Corte muito saboroso, com um osso em formato de T, sendo de um lado o contrafile e do outro o file mignon', 89, 1, 4);
+
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Sanduiche X-Tudo', 'Sandubao com muito queijo, hamburger bovino, bacon, ovo, salada e maionese', 19, 1, 5);
+
+insert into produto (nome, descricao, preco, ativo, restaurante_id) values ('Espetinho de Cupim', 'Acompanha farinha, mandioca e vinagrete', 8, 1, 6);
+
+
+insert into grupo (id, nome) values (1, 'Gerente'), (2, 'Vendedor'), (3, 'Secretaria'), (4, 'Cadastrador');
+
+insert into grupo_permissao (grupo_id, permissao_id) values (1, 1), (1, 2), (2, 1), (2, 2), (3, 1); 
+
+insert into usuario (id, nome, email, senha, data_cadastro) values
+(1, 'John Smith', 'john.ger@algafood.com.br', '123', utc_timestamp),
+(2, 'Mary Johnson', 'mary.vnd@algafood.com.br', '123', utc_timestamp),
+(3, 'Joseph Smith', 'joseph.aux@algafood.com.br', '123', utc_timestamp),
+(4, 'Sebastian McDonald', 'sebastian.cad@algafood.com.br', '123', utc_timestamp),
+(5, 'Matthew Lewis', 'email.teste.aw+matthew@algafood.com', '123', utc_timestamp),
+(6, 'Deborah Miller', 'email.teste.aw+deborah@algafood.com', '123', utc_timestamp),
+(7, 'Charles Lewis', 'email.teste.aw+charles@algafood.com', '123', utc_timestamp);
 
 insert into usuario_grupo (usuario_id, grupo_id) values (1, 1), (1, 2), (2, 2);
 
-insert into restaurante_usuario_responsavel (restaurante_id, usuario_id) values(1, 3), (3, 4);
 
-insert into pedido (id, codigo, restaurante_id, usuario_cliente_id, forma_pagamento_id, endereco_cidade_id, endereco_cep,
-    endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro,
-    status, data_criacao, subtotal, taxa_frete, valor_total)
-values (1, '65a10e1a-c7fb-43ef-9aac-7adbc5d36890', 1, 1, 1, 1, '38400-000', 'Rua Floriano Peixoto', '500', 'Apto 801',
-'Brasil', 'CRIADO', utc_timestamp, 298.90, 10, 308.90);
+insert into restaurante_usuario_responsavel (restaurante_id, usuario_id) values (1, 5), (3, 5);
+
+
+insert into pedido (id, codigo, restaurante_id, usuario_cliente_id, forma_pagamento_id, endereco_cidade_id, endereco_cep, 
+                    endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro,
+                  status, data_criacao, subtotal, taxa_frete, valor_total)
+values (1, 'f9981ca4-5a5e-4da3-af04-933861df3e55', 1, 6, 1, 1, '38400-000', 'Rua Floriano Peixoto', '500', 'Apto 801', 'Brasil',
+        'CRIADO', utc_timestamp, 298.90, 10, 308.90);
 
 insert into item_pedido (id, pedido_id, produto_id, quantidade, preco_unitario, preco_total, observacao)
 values (1, 1, 1, 1, 78.9, 78.9, null);
@@ -115,40 +109,42 @@ values (1, 1, 1, 1, 78.9, 78.9, null);
 insert into item_pedido (id, pedido_id, produto_id, quantidade, preco_unitario, preco_total, observacao)
 values (2, 1, 2, 2, 110, 220, 'Menos picante, por favor');
 
-insert into pedido (id, codigo, restaurante_id, usuario_cliente_id, forma_pagamento_id, endereco_cidade_id, endereco_cep,
-        endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro,
-        status, data_criacao, subtotal, taxa_frete, valor_total)
-values (2, '47e6fda0-8872-4516-895e-7e29031976cc', 4, 1, 2, 1, '38400-111', 'Rua Acre', '300', 'Casa 2', 'Centro',
-'CRIADO', utc_timestamp, 79, 0, 79);
+
+insert into pedido (id, codigo, restaurante_id, usuario_cliente_id, forma_pagamento_id, endereco_cidade_id, endereco_cep, 
+                    endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro,
+                  status, data_criacao, subtotal, taxa_frete, valor_total)
+values (2, 'd178b637-a785-4768-a3cb-aa1ce5a8cdab', 4, 6, 2, 1, '38400-111', 'Rua Acre', '300', 'Casa 2', 'Centro',
+        'CRIADO', utc_timestamp, 79, 0, 79);
 
 insert into item_pedido (id, pedido_id, produto_id, quantidade, preco_unitario, preco_total, observacao)
 values (3, 2, 6, 1, 79, 79, 'Ao ponto');
 
-insert into pedido (id, codigo, restaurante_id, usuario_cliente_id, forma_pagamento_id, endereco_cidade_id, endereco_cep,
+
+insert into pedido (id, codigo, restaurante_id, usuario_cliente_id, forma_pagamento_id, endereco_cidade_id, endereco_cep, 
                     endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro,
-	                status, data_criacao, data_confirmacao, data_entrega, subtotal, taxa_frete, valor_total)
-values (3, 'b5741512-8fbc-47fa-9ac1-b530354fc0ff', 1, 1, 1, 1, '38400-222', 'Rua Natal', '200', null, 'Brasil',
-        'ENTREGUE', '2023-01-30 21:10:00', '2023-01-30 21:10:45', '2023-01-30 21:55:44', 110, 10, 120);
+                  status, data_criacao, data_confirmacao, data_entrega, subtotal, taxa_frete, valor_total)
+values (3, 'b5741512-8fbc-47fa-9ac1-b530354fc0ff', 1, 7, 1, 1, '38400-222', 'Rua Natal', '200', null, 'Brasil',
+        'ENTREGUE', '2023-10-30 21:10:00', '2023-10-30 21:10:45', '2023-10-30 21:55:44', 110, 10, 120);
 
 insert into item_pedido (id, pedido_id, produto_id, quantidade, preco_unitario, preco_total, observacao)
 values (4, 3, 2, 1, 110, 110, null);
 
 
-insert into pedido (id, codigo, restaurante_id, usuario_cliente_id, forma_pagamento_id, endereco_cidade_id, endereco_cep,
+insert into pedido (id, codigo, restaurante_id, usuario_cliente_id, forma_pagamento_id, endereco_cidade_id, endereco_cep, 
                     endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro,
-	                status, data_criacao, data_confirmacao, data_entrega, subtotal, taxa_frete, valor_total)
-values (4, '5c621c9a-ba61-4454-8631-8aabefe58dc2', 1, 2, 1, 1, '38400-800', 'Rua Fortaleza', '900', 'Apto 504', 'Centro',
-        'ENTREGUE', '2023-02-02 20:34:04', '2023-02-02 20:35:10', '2023-02-02 21:10:32', 174.4, 5, 179.4);
+                  status, data_criacao, data_confirmacao, data_entrega, subtotal, taxa_frete, valor_total)
+values (4, '5c621c9a-ba61-4454-8631-8aabefe58dc2', 1, 7, 1, 1, '38400-800', 'Rua Fortaleza', '900', 'Apto 504', 'Centro',
+        'ENTREGUE', '2023-11-02 20:34:04', '2023-11-02 20:35:10', '2023-11-02 21:10:32', 174.4, 5, 179.4);
 
 insert into item_pedido (id, pedido_id, produto_id, quantidade, preco_unitario, preco_total, observacao)
 values (5, 4, 3, 2, 87.2, 174.4, null);
 
 
-insert into pedido (id, codigo, restaurante_id, usuario_cliente_id, forma_pagamento_id, endereco_cidade_id, endereco_cep,
+insert into pedido (id, codigo, restaurante_id, usuario_cliente_id, forma_pagamento_id, endereco_cidade_id, endereco_cep, 
                     endereco_logradouro, endereco_numero, endereco_complemento, endereco_bairro,
-	                status, data_criacao, data_confirmacao, data_entrega, subtotal, taxa_frete, valor_total)
+                  status, data_criacao, data_confirmacao, data_entrega, subtotal, taxa_frete, valor_total)
 values (5, '8d774bcf-b238-42f3-aef1-5fb388754d63', 1, 3, 2, 1, '38400-200', 'Rua 10', '930', 'Casa 20', 'Martins',
-        'ENTREGUE', '2023-02-03 02:00:30', '2023-02-02 21:01:21', '2023-02-03 02:20:10', 87.2, 10, 97.2);
+        'ENTREGUE', '2023-11-03 02:00:30', '2023-11-03 02:01:21', '2023-11-03 02:20:10', 87.2, 10, 97.2);
 
 insert into item_pedido (id, pedido_id, produto_id, quantidade, preco_unitario, preco_total, observacao)
 values (6, 5, 3, 1, 87.2, 87.2, null);
