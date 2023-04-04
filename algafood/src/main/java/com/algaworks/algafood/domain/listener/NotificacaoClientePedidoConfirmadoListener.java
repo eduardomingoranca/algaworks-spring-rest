7a59b4671f2252e3cdc13d4fd701e895819726e7
eduardomingoranca <eduardomingoranca@gmail.com>
2023-04-04 05:59:06 -0300

    Observando e reagindo a Domain Events

---
+++ algafood/src/main/java/com/algaworks/algafood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
@@ -0,0 +1,35 @@
+package com.algaworks.algafood.domain.listener;
+
+import com.algaworks.algafood.domain.event.PedidoConfirmadoEvent;
+import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.service.mail.EnvioEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+// componente que escuta e executa o evento
+@Component
+public class NotificacaoClientePedidoConfirmadoListener {
+    @Autowired
+    private EnvioEmailService envioEmail;
+
+    // quando pedido for confirmado vai lancar/disparar o evento
+    // o metodo sera chamado automaticamente com a instancia do evento
+    // que foi disparado
+    @EventListener
+    public void aoConfirmarPedido(PedidoConfirmadoEvent event) {
+        Pedido pedido = event.getPedido();
+        String nomeRestaurante = pedido.getRestaurante().getNome();
+        String email = pedido.getCliente().getEmail();
+
+        EnvioEmailService.Mensagem mensagem = EnvioEmailService.Mensagem.builder()
+                .assunto(nomeRestaurante + " - Pedido confirmado")
+                .corpo("pedido-confirmado.html")
+                .variavel("pedido", pedido)
+                .destinatario(email)
+                .build();
+
+        envioEmail.enviar(mensagem);
+    }
+
+}
