998f0b16c684db6b118e04808601a2cd68988342
eduardomingoranca <eduardomingoranca@gmail.com>
2023-04-07 16:25:13 -0300

    Implementando um formulario de cadastro e fazendo requisicao POST com JavaScript

--- algafood-js-client/client.js
+++ algafood-js-client/client.js
@@ -9,6 +9,39 @@ function consultar() {
     });
 }
 
+function cadastrar() {
+    // transformando o objeto em string
+    var formaPagamentoJson = JSON.stringify({
+        // obtendo o valor do campo de descricao
+        "descricao": $("#campo-descricao").val()
+    });
+    
+    console.log(formaPagamentoJson);
+
+    $.ajax({
+        url: "http://127.0.0.1:8080/formas-pagamento",
+        type: "post",
+        // payload o que sera enviado na requisicao
+        data: formaPagamentoJson,
+        contentType: "application/json",
+
+        success: function(response) {
+            alert("Forma de pagamento adicionada!");
+            consultar();
+        },
+
+        error: function(error) {
+            if (error.status == 400) {
+                // convertendo de string para javascript
+                var problem = JSON.parse(error.responseText);
+                alert(problem.userMessage);
+            } else {
+                alert("Erro ao cadastrar forma de pagamento!");
+            }
+        }
+    });
+}
+
 function preencherTabela(formasPagamento) {
     $("#tabela tbody tr").remove();
 
@@ -25,3 +58,4 @@ function preencherTabela(formasPagamento) {
 }
 
 $("#btn-consultar").click(consultar);
+$("#btn-cadastrar").click(cadastrar);
