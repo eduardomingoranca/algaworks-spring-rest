48cbfb3e8636cc311b8f885e44ec03b34bacd0d6
eduardomingoranca <eduardomingoranca@gmail.com>
2023-04-11 05:09:20 -0300

    Implementando requisicoes condicionais com Shallow ETags

--- algafood/src/main/java/com/algaworks/algafood/core/web/WebConfig.java
+++ algafood/src/main/java/com/algaworks/algafood/core/web/WebConfig.java
@@ -1,8 +1,12 @@
 package com.algaworks.algafood.core.web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -12,6 +16,16 @@ public class WebConfig implements WebMvcConfigurer {
         // habilitando o cors globalmente
         registry.addMapping("/**") // permissao de rota/caminho
                 .allowedMethods("*"); // permissao de metodos
+    }
+
+    @Bean
+    public Filter shallowEtagHeaderFilter() {
+        // ao receber uma requisicao, quando retornar uma resposta
+        // eh gerado um hash dessa resposta e coloca o cabecalho etag
+        // alem disso eh verificado se o hash dessa resposta coincide
+        // com a etag que esta no cabecalho da requisicao, caso seja
+        // verdadeiro ele utiliza o que esta em cache.
+        return new ShallowEtagHeaderFilter();
     }
 
 }
