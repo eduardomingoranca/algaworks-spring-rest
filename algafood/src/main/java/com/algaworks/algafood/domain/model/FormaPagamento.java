78bf61780319e995b46ae046fc7a27c51dee391e
eduardomingoranca <eduardomingoranca@gmail.com>
2023-04-16 06:34:20 -0300

    Entendendo e preparando a implementacao de Deep ETags

--- algafood/src/main/java/com/algaworks/algafood/domain/model/FormaPagamento.java
+++ algafood/src/main/java/com/algaworks/algafood/domain/model/FormaPagamento.java
@@ -2,11 +2,14 @@ package com.algaworks.algafood.domain.model;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+
+import java.time.OffsetDateTime;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
@@ -22,5 +25,8 @@ public class FormaPagamento {
 
     @Column(nullable = false)
     private String descricao;
+
+    @UpdateTimestamp
+    private OffsetDateTime dataAtualizacao;
 
 }
