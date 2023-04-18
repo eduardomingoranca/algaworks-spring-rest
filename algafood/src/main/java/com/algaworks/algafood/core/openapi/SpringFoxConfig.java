0a00b1d0a2d9a294d1b11cdcee087b0c1bdb71dd
eduardomingoranca <eduardomingoranca@gmail.com>
2023-04-18 07:20:41 -0300

    Descrevendo codigos de status de respostas de forma global

--- algafood/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ algafood/src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -5,11 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.List;
+
+import static java.lang.String.valueOf;
+import static java.util.Arrays.asList;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_ACCEPTABLE;
 import static springfox.documentation.builders.PathSelectors.any;
 import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
 import static springfox.documentation.spi.DocumentationType.OAS_30;
@@ -33,11 +42,27 @@ public class SpringFoxConfig {
                 .paths(any())
 //                .paths(ant("/restaurantes/*"))
                 .build()
+                .useDefaultResponseMessages(false) // deixa apenas o status de sucesso
+                .globalResponses(GET, globalGetResponseMessages())
                 .apiInfo(apiInfo())
                 .tags(firstTag);
     }
 
-    public ApiInfo apiInfo() {
+    // codigo de status padrao para o metodo GET
+    private List<Response> globalGetResponseMessages() {
+        return asList(
+                new ResponseBuilder()
+                        .code(valueOf(INTERNAL_SERVER_ERROR.value()))
+                        .description("Erro interno do Servidor")
+                        .build(),
+                new ResponseBuilder()
+                        .code(valueOf(NOT_ACCEPTABLE.value()))
+                        .description("Recurso nao possui representacao que pode ser aceita pelo consumidor")
+                        .build()
+        );
+    }
+
+    private ApiInfo apiInfo() {
         Contact contact = new Contact("Algaworks", "https://www.algaworks.com",
                 "contato@algaworks.com");
 
