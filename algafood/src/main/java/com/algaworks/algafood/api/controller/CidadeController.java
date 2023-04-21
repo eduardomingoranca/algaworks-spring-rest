1f762a48909cbc561ddfc1129bcca1fc82c83c0e
eduardomingoranca <eduardomingoranca@gmail.com>
2023-04-21 13:39:25 -0300

    Descrevendo codigos de status de respostas em endpoints especificos

--- algafood/src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ algafood/src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.api.assembler.input.disassembler.CidadeInputDisassembler;
 import com.algaworks.algafood.api.assembler.model.CidadeModelAssembler;
+import com.algaworks.algafood.api.exceptionhandler.model.Problem;
 import com.algaworks.algafood.api.model.CidadeModel;
 import com.algaworks.algafood.api.model.input.CidadeInput;
 import com.algaworks.algafood.domain.exception.EstadoNaoEncontradoException;
@@ -11,6 +12,10 @@ import com.algaworks.algafood.domain.service.CadastroCidadeService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -41,6 +46,12 @@ public class CidadeController {
     }
 
     @ApiOperation("Busca uma cidade por ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "400", description = "ID da cidade invalida.", content =
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class))),
+            @ApiResponse(responseCode = "404", description = "Cidade nao encontrada", content =
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class)))
+    })
     @GetMapping("/{cidadeId}")
     public CidadeModel buscar(@ApiParam("ID de uma cidade") @PathVariable("cidadeId") Long id) {
         Cidade cidade = cadastroCidade.buscarOuFalhar(id);
@@ -49,6 +60,7 @@ public class CidadeController {
     }
 
     @ApiOperation("Cadastra uma cidade")
+    @ApiResponses(@ApiResponse(responseCode = "201", description = "Cidade cadastrada"))
     @PostMapping
     @ResponseStatus(CREATED)
     public CidadeModel adicionar(@ApiParam(name = "corpo", value = "Representacao de uma nova cidade")
@@ -64,6 +76,11 @@ public class CidadeController {
     }
 
     @ApiOperation("Atualiza uma cidade por ID")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Cidade atualizada."),
+            @ApiResponse(responseCode = "404", description = "Cidade nao encontrada", content =
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class)))
+    })
     @PutMapping("/{cidadeId}")
     public CidadeModel atualizar(@ApiParam("ID de uma cidade")
                                  @PathVariable("cidadeId") Long id,
