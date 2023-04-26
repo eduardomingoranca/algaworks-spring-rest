51913685caa5d4e3d2a45f44a0a613a20bc6c730
eduardomingoranca <eduardomingoranca@gmail.com>
2023-04-25 21:13:08 -0300

    Desafio: descrevendo documentacao de endpoints de associacao de restaurantes com formas de pagamento

--- algafood/src/main/java/com/algaworks/algafood/api/controller/RestauranteFormaPagamentoController.java
+++ algafood/src/main/java/com/algaworks/algafood/api/controller/RestauranteFormaPagamentoController.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.api.assembler.model.FormaPagamentoModelAssembler;
 import com.algaworks.algafood.api.model.FormaPagamentoModel;
+import com.algaworks.algafood.api.openapi.controller.RestauranteFormaPagamentoControllerOpenAPI;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +11,18 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
-@RequestMapping("/restaurantes/{restauranteID}/formas-pagamento")
-public class RestauranteFormaPagamentoController {
+@RequestMapping(value = "/restaurantes/{restauranteID}/formas-pagamento", produces = APPLICATION_JSON_VALUE)
+public class RestauranteFormaPagamentoController implements RestauranteFormaPagamentoControllerOpenAPI {
     @Autowired
     private CadastroRestauranteService cadastroRestaurante;
 
     @Autowired
     private FormaPagamentoModelAssembler formaPagamentoModelAssembler;
 
+    @Override
     @GetMapping
     public List<FormaPagamentoModel> listar(@PathVariable("restauranteID") Long id) {
         Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(id);
@@ -27,12 +30,14 @@ public class RestauranteFormaPagamentoController {
         return formaPagamentoModelAssembler.toCollectionModel(restaurante.getFormasPagamento());
     }
 
+    @Override
     @DeleteMapping("/{formaPagamentoID}")
     @ResponseStatus(NO_CONTENT)
     public void desassociar(@PathVariable("restauranteID") Long id, @PathVariable Long formaPagamentoID) {
         cadastroRestaurante.desassociarFormaPagamento(id, formaPagamentoID);
     }
 
+    @Override
     @PutMapping("/{formaPagamentoID}")
     @ResponseStatus(NO_CONTENT)
     public void associar(@PathVariable("restauranteID") Long id, @PathVariable Long formaPagamentoID) {
