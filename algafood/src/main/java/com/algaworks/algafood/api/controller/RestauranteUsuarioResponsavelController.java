41623fdf770f19db9ce044f415a5c893d96be7bf
eduardomingoranca <eduardomingoranca@gmail.com>
2023-04-26 10:26:00 -0300

    Desafio: descrevendo documentacao de endpoints de associacao de restaurantes com usuarios

--- algafood/src/main/java/com/algaworks/algafood/api/controller/RestauranteUsuarioResponsavelController.java
+++ algafood/src/main/java/com/algaworks/algafood/api/controller/RestauranteUsuarioResponsavelController.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.api.assembler.model.UsuarioModelAssembler;
 import com.algaworks.algafood.api.model.UsuarioModel;
+import com.algaworks.algafood.api.openapi.controller.RestauranteUsuarioResponsavelControllerOpenAPI;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +11,18 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
-@RequestMapping("/restaurantes/{restauranteID}/responsaveis")
-public class RestauranteUsuarioResponsavelController {
+@RequestMapping(value = "/restaurantes/{restauranteID}/responsaveis", produces = APPLICATION_JSON_VALUE)
+public class RestauranteUsuarioResponsavelController implements RestauranteUsuarioResponsavelControllerOpenAPI {
     @Autowired
     private CadastroRestauranteService cadastroRestaurante;
 
     @Autowired
     private UsuarioModelAssembler usuarioModelAssembler;
 
+    @Override
     @GetMapping
     public List<UsuarioModel> listar(@PathVariable("restauranteID") Long id) {
         Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(id);
@@ -27,6 +30,7 @@ public class RestauranteUsuarioResponsavelController {
         return usuarioModelAssembler.toCollectionModel(restaurante.getUsuarios());
     }
 
+    @Override
     @PutMapping("/{usuarioID}")
     @ResponseStatus(NO_CONTENT)
     public void associar(@PathVariable("restauranteID") Long id,
@@ -34,6 +38,7 @@ public class RestauranteUsuarioResponsavelController {
         cadastroRestaurante.associarUsuarioResponsavel(id, usuarioID);
     }
 
+    @Override
     @DeleteMapping("/{usuarioID}")
     @ResponseStatus(NO_CONTENT)
     public void desassociar(@PathVariable("restauranteID") Long id,
--- algafood/src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteUsuarioResponsavelControllerOpenAPI.java
+++ algafood/src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteUsuarioResponsavelControllerOpenAPI.java
@@ -1,7 +1,7 @@
 package com.algaworks.algafood.api.openapi.controller;
 
 import com.algaworks.algafood.api.exceptionhandler.model.Problem;
-import com.algaworks.algafood.api.model.FormaPagamentoModel;
+import com.algaworks.algafood.api.model.UsuarioModel;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -13,28 +13,19 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import java.util.List;
 
 @Api(tags = "Restaurantes")
-public interface RestauranteFormaPagamentoControllerOpenAPI {
+public interface RestauranteUsuarioResponsavelControllerOpenAPI {
 
-    @ApiOperation("Lista as formas de pagamento associadas a restaurante")
+    @ApiOperation("Lista os usuarios associadas a restaurante")
     @ApiResponses({
             @ApiResponse(responseCode = "400", description = "Codigo do restaurante invalido.", content =
             @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class))),
             @ApiResponse(responseCode = "404", description = "Restaurante nao encontrado", content =
             @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class)))
     })
-    List<FormaPagamentoModel> listar(@ApiParam(value = "Codigo de um restaurante", required = true) Long id);
+    List<UsuarioModel> listar(@ApiParam(value = "Codigo de um restaurante", required = true) Long id);
 
-    @ApiOperation("Desassociacao de restaurante com forma de pagamento")
-    @ApiResponses({
-            @ApiResponse(responseCode = "400", description = "Codigo do restaurante invalido.", content =
-            @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class))),
-            @ApiResponse(responseCode = "404", description = "Restaurante nao encontrado", content =
-            @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class)))
-    })
-    void desassociar(@ApiParam(value = "Codigo de um restaurante", required = true) Long id,
-                     @ApiParam(value = "Codigo da forma de pagamento", required = true) Long formaPagamentoID);
 
-    @ApiOperation("Associacao de restaurante com forma de pagamento")
+    @ApiOperation("Associacao de restaurante com usuario responsavel")
     @ApiResponses({
             @ApiResponse(responseCode = "400", description = "Codigo do restaurante invalido.", content =
             @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class))),
@@ -42,6 +33,17 @@ public interface RestauranteFormaPagamentoControllerOpenAPI {
             @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class)))
     })
     void associar(@ApiParam(value = "Codigo de um restaurante", required = true) Long id,
-                  @ApiParam(value = "Codigo da forma de pagamento", required = true) Long formaPagamentoID);
+                  @ApiParam(value = "Codigo do usuario responsavel", required = true) Long usuarioID);
+
+
+    @ApiOperation("Desassociacao de restaurante com usuario responsavel")
+    @ApiResponses({
+            @ApiResponse(responseCode = "400", description = "Codigo do restaurante invalido.", content =
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class))),
+            @ApiResponse(responseCode = "404", description = "Restaurante nao encontrado", content =
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class)))
+    })
+    void desassociar(@ApiParam(value = "Codigo de um restaurante", required = true) Long id,
+                     @ApiParam(value = "Codigo do usuario responsavel", required = true) Long usuarioID);
 
 }
