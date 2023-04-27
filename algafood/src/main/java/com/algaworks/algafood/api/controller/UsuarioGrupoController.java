bf56b12ebccb1b177d77dd072bf014c0f71d58f1
eduardomingoranca <eduardomingoranca@gmail.com>
2023-04-27 10:45:44 -0300

    Desafio: descrevendo documentacao de endpoints de associacao de grupos com usuarios

--- algafood/src/main/java/com/algaworks/algafood/api/controller/UsuarioGrupoController.java
+++ algafood/src/main/java/com/algaworks/algafood/api/controller/UsuarioGrupoController.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.api.assembler.model.GrupoModelAssembler;
 import com.algaworks.algafood.api.model.GrupoModel;
+import com.algaworks.algafood.api.openapi.controller.UsuarioGrupoControllerOpenAPI;
 import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.service.CadastroUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,16 +11,18 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 import static org.springframework.http.HttpStatus.NO_CONTENT;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
-@RequestMapping("/usuarios/{usuarioID}/grupos")
-public class UsuarioGrupoController {
+@RequestMapping(value = "/usuarios/{usuarioID}/grupos", produces = APPLICATION_JSON_VALUE)
+public class UsuarioGrupoController implements UsuarioGrupoControllerOpenAPI {
     @Autowired
     private CadastroUsuarioService cadastroUsuario;
 
     @Autowired
     private GrupoModelAssembler grupoModelAssembler;
 
+    @Override
     @GetMapping
     public List<GrupoModel> listar(@PathVariable("usuarioID") Long id) {
         Usuario usuario = cadastroUsuario.buscarOuFalhar(id);
@@ -27,6 +30,7 @@ public class UsuarioGrupoController {
         return grupoModelAssembler.toCollectionModel(usuario.getGrupos());
     }
 
+    @Override
     @PutMapping("/{grupoID}")
     @ResponseStatus(NO_CONTENT)
     public void associar(@PathVariable("usuarioID") Long id,
@@ -34,6 +38,7 @@ public class UsuarioGrupoController {
         cadastroUsuario.associarGrupo(id, grupoID);
     }
 
+    @Override
     @DeleteMapping("/{grupoID}")
     @ResponseStatus(NO_CONTENT)
     public void desassociar(@PathVariable("usuarioID") Long id,
---
+++ algafood/src/main/java/com/algaworks/algafood/api/openapi/controller/UsuarioGrupoControllerOpenAPI.java
@@ -0,0 +1,47 @@
+package com.algaworks.algafood.api.openapi.controller;
+
+import com.algaworks.algafood.api.exceptionhandler.model.Problem;
+import com.algaworks.algafood.api.model.GrupoModel;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+import java.util.List;
+
+@Api(tags = "Usuarios")
+public interface UsuarioGrupoControllerOpenAPI {
+
+    @ApiOperation("Lista os grupos associadas ao usuario")
+    @ApiResponses({
+            @ApiResponse(responseCode = "400", description = "Codigo do usuario invalido.", content =
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class))),
+            @ApiResponse(responseCode = "404", description = "Usuario nao encontrado", content =
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class)))
+    })
+    List<GrupoModel> listar(@ApiParam(value = "Codigo de um usuario", required = true) Long id);
+
+    @ApiOperation("Associacao de usuario com grupo")
+    @ApiResponses({
+            @ApiResponse(responseCode = "400", description = "Codigo do usuario invalido", content =
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class))),
+            @ApiResponse(responseCode = "404", description = "Usuario nao encontrado", content =
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class)))
+    })
+    void associar(@ApiParam(value = "Codigo de um usuario", required = true) Long id,
+                  @ApiParam(value = "Codigo do grupo", required = true) Long grupoID);
+
+    @ApiOperation("Desassociacao de usuario com grupo")
+    @ApiResponses({
+            @ApiResponse(responseCode = "400", description = "Codigo do usuario invalido", content =
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class))),
+            @ApiResponse(responseCode = "404", description = "Usuario nao encontrado", content =
+            @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class)))
+    })
+    void desassociar(@ApiParam(value = "Codigo de um usuario", required = true) Long id,
+                     @ApiParam(value = "Codigo do grupo", required = true) Long grupoID);
+
+}
