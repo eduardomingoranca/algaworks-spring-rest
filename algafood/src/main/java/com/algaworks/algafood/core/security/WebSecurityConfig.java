bb7af662ad62491d910a57e459c3271867d24e98
eduardomingoranca <eduardomingoranca@gmail.com>
2023-05-13 13:45:01 -0300

    Configurando autenticacao de usuarios em memoria

--- algafood/src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
+++ algafood/src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
@@ -1,15 +1,33 @@
 package com.algaworks.algafood.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        String encodePassword = passwordEncoder().encode("123");
+
+        auth.inMemoryAuthentication()
+                .withUser("edward")
+                .password(encodePassword)
+                .roles("ADMIN")
+                .and()
+                .withUser("john")
+                .password(encodePassword)
+                .roles("ADMIN");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -27,6 +45,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // CSRF eh um tipo de ataque onde quem esta atacando falsifica a requisicao, isso
                 // acontece quando se eh utilizado cookie.
                 .csrf().disable();
+    }
+
+    // criptografia de senha
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
