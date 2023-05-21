9c2b3463c376fb2753164f916c6dbef52bd45762
eduardomingoranca <eduardomingoranca@gmail.com>
2023-05-21 14:31:26 -0300

    Implementando o suporte a PKCE com o fluxo Authorization Code

--- algafood-auth/src/main/java/com/algaworks/algafood/auth/config/AuthorizationServerConfig.java
+++ algafood-auth/src/main/java/com/algaworks/algafood/auth/config/AuthorizationServerConfig.java
@@ -10,6 +10,12 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.CompositeTokenGranter;
+import org.springframework.security.oauth2.provider.TokenGranter;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 @Configuration
 @EnableAuthorizationServer
@@ -82,7 +88,19 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .authenticationManager(authenticationManager)
                 .userDetailsService(userDetailsService)
                 // nao reutiza o refresh token
-                .reuseRefreshTokens(false);
+                .reuseRefreshTokens(false)
+                .tokenGranter(tokenGranter(endpoints));
+    }
+
+    private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
+        PkceAuthorizationCodeTokenGranter pkceAuthorizationCodeTokenGranter = new PkceAuthorizationCodeTokenGranter(
+                endpoints.getTokenServices(), endpoints.getAuthorizationCodeServices(),
+                endpoints.getClientDetailsService(), endpoints.getOAuth2RequestFactory());
+
+
+        List<TokenGranter> granters = asList(pkceAuthorizationCodeTokenGranter, endpoints.getTokenGranter());
+
+        return new CompositeTokenGranter(granters);
     }
 
 }
