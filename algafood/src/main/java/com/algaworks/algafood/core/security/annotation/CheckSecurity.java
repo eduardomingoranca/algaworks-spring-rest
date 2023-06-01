56b199b97f4901cbc925af3e8d6b836f06333755
eduardomingoranca <eduardomingoranca@gmail.com>
2023-06-01 20:12:57 -0300

    Corrigindo logica de restricao de acessos para Client Credentials Flow

--- algafood/src/main/java/com/algaworks/algafood/core/security/annotation/CheckSecurity.java
+++ algafood/src/main/java/com/algaworks/algafood/core/security/annotation/CheckSecurity.java
@@ -52,14 +52,14 @@ public @interface CheckSecurity {
 
         @PreAuthorize("hasAuthority('SCOPE_READ') and isAuthenticated()")
         @PostAuthorize("hasAuthority('CONSULTAR_PEDIDOS') or " +
-                "@algaSecurity.getUsuarioId() == returnObject.cliente.id or " +
+                "@algaSecurity.usuarioAutenticadoIgual(returnObject.cliente.id) or " +
                 "@algaSecurity.gerenciaRestaurante(returnObject.restaurante.id)")
         @Retention(RUNTIME)
         @Target(METHOD)
         @interface PodeBuscar { }
 
         @PreAuthorize("hasAuthority('SCOPE_READ') and (hasAuthority('CONSULTAR_PEDIDOS') or "
-                + "@algaSecurity.getUsuarioId() == #filtro.clienteId or"
+                + "@algaSecurity.usuarioAutenticadoIgual(#filtro.clienteId) or"
                 + "@algaSecurity.gerenciaRestaurante(#filtro.restauranteId))")
         @Retention(RUNTIME)
         @Target(METHOD)
@@ -119,13 +119,13 @@ public @interface CheckSecurity {
 
     @interface UsuariosGruposPermissoes {
         @PreAuthorize("hasAuthority('SCOPE_WRITE') and "
-                + "@algaSecurity.getUsuarioId() == #usuarioId")
+                + "@algaSecurity.usuarioAutenticadoIgual(#usuarioId)")
         @Retention(RUNTIME)
         @Target(METHOD)
         @interface PodeAlterarPropriaSenha { }
 
         @PreAuthorize("hasAuthority('SCOPE_WRITE') and (hasAuthority('EDITAR_USUARIOS_GRUPOS_PERMISSOES') or "
-                + "@algaSecurity.getUsuarioId() == #usuarioId)")
+                + "@algaSecurity.usuarioAutenticadoIgual(#usuarioId))")
         @Retention(RUNTIME)
         @Target(METHOD)
         @interface PodeAlterarUsuario { }
