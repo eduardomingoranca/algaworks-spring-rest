beab9cba047e569bf89ffc90c418ecbf2d8e86d0
eduardomingoranca <eduardomingoranca@gmail.com>
2023-06-08 20:32:25 -0300

    Descrevendo as operacoes de endpoints na documentacao

--- algafood/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenAPI.java
+++ algafood/src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenAPI.java
@@ -4,6 +4,7 @@ package com.algaworks.algafood.api.v1.openapi.controller;
 import com.algaworks.algafood.api.exceptionhandler.model.Problem;
 import com.algaworks.algafood.api.v1.model.CidadeModel;
 import com.algaworks.algafood.api.v1.model.input.CidadeInput;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -16,8 +17,10 @@ import org.springframework.hateoas.CollectionModel;
 @Tag(name = "Cidades")
 public interface CidadeControllerOpenAPI {
 
+    @Operation(summary = "Lista as cidades")
     CollectionModel<CidadeModel> listar();
 
+    @Operation(summary = "Busca uma cidade por ID")
     @ApiResponses({
             @ApiResponse(responseCode = "400", description = "ID da cidade invalida.", content =
             @Content(mediaType = "application/json", schema = @Schema(implementation = Problem.class))),
@@ -26,9 +29,12 @@ public interface CidadeControllerOpenAPI {
     })
     CidadeModel buscar(Long id);
 
+    @Operation(summary = "Cadastra uma cidade")
     @ApiResponses(@ApiResponse(responseCode = "201", description = "Cidade cadastrada"))
     CidadeModel adicionar(CidadeInput cidadeInput);
 
+    @Operation(summary = "Atualizado uma cidade por ID", description = "Cadastro de uma cidade, " +
+            "necessita de um estado e um nome valido")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "Cidade atualizada."),
             @ApiResponse(responseCode = "404", description = "Cidade nao encontrada", content =
