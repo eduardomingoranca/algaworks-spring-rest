97493e8f612a6a5a1b2f9308ab19de45f73f3f7e
eduardomingoranca <eduardomingoranca@gmail.com>
2023-06-08 10:20:47 -0300

    Entendendo o Poor Man's Load Balancer (DNS Round Robin)

---
+++ algafood/src/main/java/com/algaworks/algafood/api/HostCheckController.java
@@ -0,0 +1,20 @@
+package com.algaworks.algafood.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.UnknownHostException;
+
+import static java.net.InetAddress.getLocalHost;
+
+@RestController
+public class HostCheckController {
+
+    @GetMapping("/hostcheck")
+    public String checkHost() throws UnknownHostException {
+        // obtendo o host/ip do container que responde a requisicao
+        return getLocalHost().getHostAddress()
+                + " - " + getLocalHost().getHostName();
+    }
+
+}
