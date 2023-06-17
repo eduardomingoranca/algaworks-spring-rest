3fe13da571c59ffa61809e23e97d29a129e0bb97
eduardomingoranca <eduardomingoranca@gmail.com>
2023-06-17 17:15:56 -0300

    Lendo informacoes customizadas do JWT no Resource Server

--- algafood/src/main/java/com/algaworks/algafood/core/security/config/resourceserver/ResourceServerConfig.java
+++ algafood/src/main/java/com/algaworks/algafood/core/security/config/resourceserver/ResourceServerConfig.java
@@ -5,8 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.Collection;
+import java.util.List;
+
+import static java.util.Collections.emptyList;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -22,10 +30,32 @@ public class ResourceServerConfig {
                 .and()
                 .csrf().disable()
                 .cors().and()
-                .oauth2ResourceServer().jwt();
+                .oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
 
         return http.formLogin(withDefaults()).build();
     }
 
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            List<String> authorities = jwt.getClaimAsStringList("authorities");
+
+            if (authorities == null)
+                return emptyList();
+
+            JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+            Collection<GrantedAuthority> grantedAuthorities = authoritiesConverter.convert(jwt);
+
+            grantedAuthorities.addAll(authorities
+                    .stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .toList());
+
+            return grantedAuthorities;
+        });
+
+        return converter;
+    }
 
 }
