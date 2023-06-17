50af8fb045b2fd4f9fc9410c916a5f79d43e6240
eduardomingoranca <eduardomingoranca@gmail.com>
2023-06-17 10:36:47 -0300

    Configuracao inicial do Authorization Server com Access Token opaco

---
+++ algafood/src/main/java/com/algaworks/algafood/core/security/authorizationserver/config/AuthorizationServerConfig.java
@@ -0,0 +1,62 @@
+package com.algaworks.algafood.core.security.authorizationserver.config;
+
+import com.algaworks.algafood.core.security.authorizationserver.config.property.AlgafoodSecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
+import org.springframework.security.oauth2.server.authorization.config.TokenSettings;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static java.time.Duration.ofMinutes;
+import static java.util.Collections.singletonList;
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
+import static org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration.applyDefaultSecurity;
+import static org.springframework.security.oauth2.core.AuthorizationGrantType.CLIENT_CREDENTIALS;
+import static org.springframework.security.oauth2.core.ClientAuthenticationMethod.CLIENT_SECRET_BASIC;
+import static org.springframework.security.oauth2.core.OAuth2TokenFormat.REFERENCE;
+import static org.springframework.security.oauth2.server.authorization.client.RegisteredClient.withId;
+
+@Configuration
+public class AuthorizationServerConfig {
+
+    // aplicando os filtros de securanca
+    @Bean
+    @Order(HIGHEST_PRECEDENCE)
+    public SecurityFilterChain authFilterChain(HttpSecurity httpSecurity) throws Exception {
+        applyDefaultSecurity(httpSecurity);
+        return httpSecurity.build();
+    }
+
+    // responsavel por escrever o authorization server que vai assinar os tokens
+    @Bean
+    public ProviderSettings providerSettings(AlgafoodSecurityProperties properties) {
+        return ProviderSettings.builder()
+                .issuer(properties.getProviderUrl())
+                .build();
+    }
+
+    // responsavel por guardar os clients do authorization server
+    @Bean
+    public RegisteredClientRepository registeredClientRepository(PasswordEncoder passwordEncoder) {
+        RegisteredClient algafoodBackend = withId("1")
+                .clientId("algafood-backend")
+                .clientSecret(passwordEncoder.encode("backend123"))
+                .clientAuthenticationMethod(CLIENT_SECRET_BASIC)
+                .authorizationGrantType(CLIENT_CREDENTIALS)
+                .scope("READ")
+                .tokenSettings(TokenSettings.builder()
+                        .accessTokenFormat(REFERENCE)
+                        .accessTokenTimeToLive(ofMinutes(30))
+                        .build())
+                .build();
+
+        return new InMemoryRegisteredClientRepository(singletonList(algafoodBackend));
+    }
+
+}
