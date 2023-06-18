3f24d3d98dd9508381843d475e3f08ae653daa24
eduardomingoranca <eduardomingoranca@gmail.com>
2023-06-18 13:18:37 -0300

    Atualizando o Spring Doc

--- algafood/src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
+++ algafood/src/main/java/com/algaworks/algafood/core/springdoc/SpringDocConfig.java
@@ -16,7 +16,7 @@ import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.tags.Tag;
-import org.springdoc.core.customizers.OpenApiCustomiser;
+import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -108,7 +108,7 @@ public class SpringDocConfig {
     }
 
     @Bean
-    public OpenApiCustomiser openApiCustomiser() {
+    public OpenApiCustomizer openApiCustomiser() {
         return openApi -> openApi.getPaths()
                 .values()
                 .forEach(pathItem -> pathItem.readOperationsMap()
